45318df7317f2a81b970757ef41f2cf57e33f25b
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-09-16 14:59:45 +0300

    cleanup

--- bin/build.js
+++ bin/build.js
@@ -7,7 +7,6 @@ const DEFAULT_DOMAIN = "default";
 require("./../../privatesky/psknode/bundles/csbBoot.js");
 require("./../../privatesky/psknode/bundles/openDSU.js");
 const fs = require("fs");
-const EDFS = require("edfs");
 const openDSU = require("opendsu");
 const bdns = openDSU.loadApi("bdns");
 const keyssi = openDSU.loadApi("keyssi");
@@ -16,8 +15,6 @@ bdns.addRawInfo(DEFAULT_DOMAIN, {
     brickStorages: [BRICK_STORAGE_ENDPOINT],
     anchoringServices: [BRICK_STORAGE_ENDPOINT]
 })
-
-let APP_CONFIG = {};
 
 function getCardinalDossierSeed(callback){
     fs.readFile(CARDINAL_SEED_FILE_PATH, (err, content)=>{
